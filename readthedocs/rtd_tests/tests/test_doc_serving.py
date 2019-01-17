da69ba0156a30740fe1606b4c91306532b398563
Manuel Kaufmann <humitos@gmail.com>
2019-01-17 15:31:42 +0100

    Test case for public projects fixed

--- readthedocs/rtd_tests/tests/test_doc_serving.py
+++ readthedocs/rtd_tests/tests/test_doc_serving.py
@@ -15,6 +15,7 @@ from django.conf import settings
 from django.urls import reverse
 
 from readthedocs.builds.constants import STABLE
+from readthedocs.builds.models import Version
 from readthedocs.rtd_tests.base import RequestFactoryTestMixin
 from readthedocs.projects import constants
 from readthedocs.projects.models import Project
@@ -203,32 +204,35 @@ class TestPublicDocs(BaseDocServing):
         PUBLIC_DOMAIN='readthedocs.io',
         ROOT_URLCONF=settings.SUBDOMAIN_URLCONF,
     )
-    def test_default_robots_txt(self):
-        self.public.versions.update(active=True, built=True)
-        self.public.versions.filter(slug=STABLE).update(privacy_level=constants.PRIVATE)
+    def test_sitemap_xml(self):
+        self.public.versions.update(active=True)
+        private_version = fixture.get(
+            Version,
+            privacy_level=constants.PRIVATE,
+            project=self.public,
+        )
         response = self.client.get(
             reverse('sitemap_xml'),
             HTTP_HOST='public.readthedocs.io',
         )
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content_type, 'application/xml')
+        self.assertEqual(response['Content-Type'], 'application/xml')
         for version in self.public.versions.filter(privacy_level=constants.PUBLIC):
             self.assertContains(
                 response,
                 self.public.get_docs_url(
                     version_slug=version.slug,
-                    lang_slug=self.project.language,
+                    lang_slug=self.public.language,
                     private=False,
                 ),
             )
 
         # stable is marked as PRIVATE and should not appear here
-        stable = self.public.versions.get(slug=STABLE)
         self.assertNotContains(
             response,
             self.public.get_docs_url(
-                version_slug=stable.slug,
-                lang_slug=self.project.language,
+                version_slug=private_version.slug,
+                lang_slug=self.public.language,
                 private=True,
             ),
         )
